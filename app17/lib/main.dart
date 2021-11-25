c2a30bdc42e59b76686690b2ad2340ece25c1aea
Andriw Felipe Costa de Lima Santos <54964937+andriwf@users.noreply.github.com>
2021-11-24 23:52:12 -0300

    app17 - CEP

---
+++ app17/lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Home()
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController cep = new TextEditingController();
+  String inf = '';
+
+  findMe() async {
+    String url = 'https://viacep.com.br/ws/${cep.text}/json/';
+
+    http.Response ans = await http.get(Uri.parse(url));
+
+    Map<String, dynamic> back = json.decode(ans.body);
+    String logradouro = back['logradouro'];
+    String bairro = back['bairro'];
+    String localidade = back['localidade'];
+    String uf = back['uf'];
+
+    setState(() {
+      inf = '$logradouro \n$bairro - $localidade ($uf)';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: title(),
+        body: body(),
+      backgroundColor: Colors.amber,
+    );
+  }
+
+  title(){
+    return AppBar(
+        title: Text('Encontra Endereço', style: TextStyle(fontSize:20, color: Colors.black)),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue
+    );
+  }
+
+  body(){
+    return Container(
+        margin: EdgeInsets.all(20),
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Column(
+            children: <Widget>[
+              Container(
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child:tf(TextInputType.number, cep, 'CEP')),
+              Container(
+                  width: 300,
+                  height: 50,
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child:bt()),
+              Container(
+                  width: 300,
+                  height: 200,
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child: Text(inf,
+                      style: TextStyle(fontSize:20, color: Colors.black))
+              )
+            ]
+        )
+    );
+  }
+
+  bt(){
+    return ElevatedButton(
+        child: Text('Encontrar', style: TextStyle(fontSize:20, color: Colors.black)),
+        onPressed: findMe,
+        style: ButtonStyle(
+            fixedSize: MaterialStateProperty.all(Size.fromRadius(double.maxFinite)),
+            backgroundColor: MaterialStateProperty.all(Colors.lightBlue))
+    );
+  }
+
+  tf(keyType, controll, txt){
+    return TextField(
+        keyboardType: keyType,
+        controller: controll,
+        decoration: InputDecoration(
+            labelText: txt,
+            labelStyle: TextStyle(color: Colors.lightBlue)),
+        style: TextStyle(color: Colors.lightBlue, fontSize:40)
+    );
+  }
+}
